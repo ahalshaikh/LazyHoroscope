58b3e8dccb167ad2a1ce52473b21245e48965e5a
ahalshaikh <ahalshaikh@mail.csuchico.edu>
2018-02-12 17:15:17 -0800

    asks for first and last name

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@ using namespace std;
 int main()
 {
   //declare variables
+  string name, lastname;
+  
 
   //get user input
+  cout<<"please enter your first and last name: ";
+cin>>name, lastname;
 
   //tell fortune
 
