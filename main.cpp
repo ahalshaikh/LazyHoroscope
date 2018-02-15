64b3af2915f72ee93656d700d5ac06d7b5b36382
ahalshaikh <ahalshaikh@mail.csuchico.edu>
2018-02-14 17:42:09 -0800

    tell you your fortune and your destiny

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ cout<<"Welcome, "<<nickname<<", here is your fortune..."<<endl;
 int lucky;
 lucky = name.length();
 cout<<"your lucky number is: "<<lucky<<endl;
-if ((name[0] == 'a') || (name[0] == 'e') || (name[0] == 'i') || (name[0] == 'u')|| (name[0] == 'A') || (name[0] == 'E') || (name[0] == 'I') || (name[0] == 'U'))
+if ((name[0] == 'a') || (name[0] == 'e') || (name[0] == 'i') || (name[0] == 'o')|| (name[0] == 'u')|| (name[0] == 'A') || (name[0] == 'E') || (name[0] == 'I') || (name[0] =='O') || (name[0] == 'U'))
 {
 cout<<"you are destined to be famous! \nyou have already met your true love. \nhave a good day!"<<endl;
 }
